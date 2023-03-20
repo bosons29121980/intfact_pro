313017c5965d33833e01ddbf60df21d2e10eab82
Subhendra Basu <basu.subhendra@outlook.com>
2023-03-20 15:42:11 +0530

    code

--- common.h
+++ common.h
@@ -6,11 +6,6 @@
 #include <stdlib.h>
 using namespace std;
 
-struct arg {
-    char* num;
-    int param;
-};
-
 char* _int_(char* r) {
     mpz_t prod;
     mpz_init(prod);
@@ -34,94 +29,4 @@ char* _int_(char* r) {
     return ret;
 }
 
-int decision(char* d1, char* d2, char* num) {
-    mpz_t dz1;
-    mpz_init(dz1);
-    mpz_t dz2;
-    mpz_init(dz2);
-    mpz_set_str(dz1, d1, 10);
-    mpz_set_str(dz2, d2, 10);
-    mpz_t res;
-    mpz_init(res);
-    mpz_mul(res, dz1, dz2);
-    mpz_t nz;
-    mpz_init(nz);
-    mpz_set_str(nz, num, 10);
-    if (mpz_cmp(res, nz)  == 0) {
-          mpz_clear(dz1);
-          mpz_clear(dz2);
-          mpz_clear(res);
-          mpz_clear(nz);
-          return 0;
-    } else {
-          mpz_clear(dz1);
-          mpz_clear(dz2);
-          mpz_clear(res);
-          mpz_clear(nz);
-          return 1;
-    }
-}
-
-char* interpret(std::string result, int param, int p, char* num, int& is_prime) {
-    if (param == 1) {
-       std::reverse(result.begin(), result.end());
-    }
-    char* ret = (char*) calloc(result.size() + 1, sizeof(char));
-    for (int i = 0; i < result.size(); ++i) {
-          if (p == 1) {
-               if (result[i] == 'A') {
-                     ret[i] = '1';
-               } else {
-                     ret[i] = '0';
-               }
-          } else {
-               if (result[i] == 'A') {
-                     ret[i] = '0';
-               } else {
-                     ret[i] = '1';
-               }
-          }
-    }
-    ret[result.size()] = '\0';
-    char* dec = _int_(ret);
-    mpz_t dz;
-    mpz_init(dz);
-    mpz_set_str(dz, dec, 10);
-    mpz_t nz;
-    mpz_init(nz);
-    mpz_set_str(nz, num, 10);
-    if (mpz_cmp(dz, nz) >= 0) {
-         is_prime = 1;
-    } else {
-         is_prime = 0;
-    }
-    mpz_clear(dz);
-    mpz_clear(nz);
-    return dec;
-}
-
-char* _modulo_(char* num, char* x, int& d) {
-   mpz_t nz;
-   mpz_init(nz);
-   mpz_set_str(nz, num, 10);
-   mpz_t xz;
-   mpz_init(xz);
-   mpz_set_str(xz, x, 10);
-   mpz_t remainder;
-   mpz_init(remainder);
-   char* r = 0;
-   if (mpz_cmp_si(xz, 0) <= 0) {
-	   r = strdup("-1");
-   } else {
-	   mpz_mod(remainder, nz, xz);
-	   r =  strdup(mpz_get_str(0, 10, remainder));
-   }
-   if (mpz_cmp_si(xz, 1) <= 0 || mpz_cmp(xz, nz) == 0) {
-        d = 1;
-   }
-   mpz_clear(nz);
-   mpz_clear(xz);
-   mpz_clear(remainder);
-   return r;
-}
 #endif
--- intfact.cpp
+++ intfact.cpp
@@ -8,22 +8,6 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
      char* num = strdup(argv[1]);
-     char* ret1 = 0, *ret2 = 0;
-     pthread_t thread1, thread2;
-     struct arg arg1;
-     struct arg arg2;
-     arg1.num = num;
-     arg2.num = num;
-     arg1.param = 1;
-     arg2.param = 0;
-     pthread_create(&thread1, NULL, factorize, &arg1);
-//     pthread_create(&thread2, NULL, factorize, &arg2);
-     pthread_join(thread1, (void**) &ret1);
- //    pthread_join(thread2, (void**) &ret2);
-     if ((ret1 != 0 && strcmp(ret1, "Prime") == 0) && (ret2 != 0 && strcmp(ret2, "Prime") == 0)) {
-           printf(" %s is a prime number. \n", num);
-     } else {
-           printf("%s = %s X %s\n", num, ret1, ret2);
-     }
+     free(num);
      return 0;
 }
