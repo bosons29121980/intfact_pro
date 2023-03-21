95192bea815c5ed824dc1e699c55cc7e8127907d
Subhendra Basu <basu.subhendra@outlook.com>
2023-03-21 12:18:11 +0530

    code

--- factorize.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FACTORIZE__
-#define __FACTORIZE__
-#include <iostream>
-#include <stdio.h>
-#include <string>
-#include "common.h"
-using namespace std;
-
-#endif
--- intfact.py
+++ intfact.py
@@ -2,29 +2,31 @@
 import sys
 
 if __name__ == "__main__":
-	num = str(sys.argv[1])
-        l = len(num)
-        p = int(sys.argv[2])
-        if p == 0:
-	    f=open("./pi.dat","r")
-        else:
-	    f=open("./e.dat","r")
-        ctr = 0
-        c = ""
+    num = str(sys.argv[1])
+    l = len(num)
+    p = int(sys.argv[2])
+    if p == 0:
+        f=open("./pi.dat","r")
+    else:
+        f=open("./e.dat","r")
+    f.read(2)
+    ctr = 0
+    c = ""
+    while True:
+        n = num[ctr]
         while True:
-            n = num[ctr]
-            while True:
-                c = str(f.read(l))
-                if c[ctr] == n:
-                   blk = c
-                   break
-            print(blk)
-            k=int(input('get next?'))
-            if k == 0:
-               break
-            ctr = ctr + 1
-            while c[ctr] == n:
-                input("same")
-                ctr = ctr + 1
-	f.read(2)
-	f.close()
+            c = str(f.read(l))
+            print(c)
+            if c[ctr] == n:
+                break
+        k=int(input('get next?'))
+        if k == 0:
+            break
+        ctr = (ctr + 1) % l
+        n = num[ctr]
+        while c[ctr] == n:
+            print(c)
+            input("same")
+            ctr = (ctr + 1) % l
+    f.read(2)
+    f.close()
