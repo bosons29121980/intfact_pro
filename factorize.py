53f008a3fd9309f2c3f1ccb16a82f2e863de9ed6
Subhendra Basu <basu.subhendra@outlook.com>
2023-03-21 12:49:39 +0530

    code

--- factorize.py
+++ factorize.py
@@ -6,5 +6,16 @@ def factorize(num, param, factors):
     factor = ""
     c = ""
     ctr = 0
+    if param == 0:
+        f = open("./e.dat", "r")
+        g = open("./pi.dat","r")
+    else:
+        f = open("./pi.dat", "r")
+        g = open("./e.dat", "r")
+    f.read(2)
+    g.read(2)
     while True:
-        c, ctr = get_next_block(num, param, c, ctr)
+        position, ctr = get_next_block(num, 1 - param, c, ctr)
+        f.seek(position)
+        while True:
+            pass
