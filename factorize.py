0c01fc8225ac53db2173195d6b508042c6abc435
Subhendra Basu <basu.subhendra@outlook.com>
2023-03-22 09:59:45 +0530

    code

--- factorize.py
+++ factorize.py
@@ -4,21 +4,22 @@ from mpmath import mp
 from mpmath import zetazero
 import sys
 
-BASE = 64
-def get_zero(zero_index, l):
-    global BASE
-    mp.prec = BASE + 2*l
-    mp.dps = BASE + 2*l
+def either_8_or_7(x):
+    if x == '7' or x == '8':
+        return True
+    return False
+
+def get_zero(zero_index, base):
+    mp.prec = base
+    mp.dps = base
     zero = str(zetazero(zero_index).imag)
     idx = zero.index(".")
     zero = zero[idx + 1:]
     return zero
 
-def factorize(num, param, factors):
+def factorize(num, param, factors, base):
     l = len(num)
     factor = ""
-    snippet = ""
-    c = ""
     ctr = 0
     if param == 0:
         f = open("./e.dat", "r")
@@ -36,12 +37,27 @@ def factorize(num, param, factors):
             continue
         else:
             f.seek(position)
-            pp = ""
+            zero = get_zero(zero_index, base)
+            cnt = 0
+            z = len(zero)
+            n87 = 0
             while True:
                 c = str(f.read(1))
-                pp = pp + c
-                d = int(input(pp))
-                if d == 0:
+                d = zero[cnt]
+                cnt = cnt + 1
+                if cnt > z:
+                    print("Out of Precision Exception !!")
+                    sys.exit(2)
+                b87c = either_8_or_7(c)
+                b87d = either_8_or_7(d)
+                if  b87c == True and  == True:
+                    if c == '8' and d == '7':
+                        factor = factor + str(bin(n87)[2:]) 
                     break
-            print(pp)
+                if b87c == True:
+                    n87 = n87 + 1 
             zero_index = zero_index + 1
+    f.close()
+    if len(factor) > 0:
+       dec = int(factor, 2)
+       return dec
--- intfact.py
+++ intfact.py
@@ -5,11 +5,12 @@ from factorize import factorize
 
 if __name__ == "__main__":
      num = str(sys.argv[1])
+     prec = int(sys.argv[2])
      factors = dict([])
      factors[0] = 0
      factors[1] = 0
-     t1 = threading.Thread(target=factorize, args=(num, 0, factors,))
-     #t2 = threading.Thread(target=factorize, args=(num, 1, factors,))
+     t1 = threading.Thread(target=factorize, args=(num, 0, factors, prec,))
+     #t2 = threading.Thread(target=factorize, args=(num, 1, factors, prec,))
      t1.start()
      #t2.start()
      #if factors[0]*factors[1] == num:
