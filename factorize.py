2cef5d0a0180ed02759918125498e4e1a9864d39
Subhendra Basu <basu.subhendra@outlook.com>
2023-03-22 15:01:23 +0530

    code

--- factorize.py
+++ factorize.py
@@ -10,90 +10,68 @@ def either_8_or_7(x):
         return True
     return False
 
+zeros_hash = dict([])
 def get_zero(zero_index, base):
-    mp.prec = base
-    mp.dps = base
-    zero = str(zetazero(zero_index).imag)
-    idx = zero.index(".")
-    zero = zero[idx + 1:]
-    return zero
-
-def divides(num, d):
-    nz = gmpy2.mpz(num)
-    dz = gmpy2.mpz(d)
-    if dz <= gmpy2.mpz("1"):
-         return False, True
-    if dz >= nz:
-         return False, False
-    r = gmpy2.f_mod(nz, dz)
-    if r == 0:
-         return True, True
-    return False, True
-    
-def factorize(num, param, factors, base):
-    l = len(num)
-    factor = ""
-    snippet = ""
-    ctr = 0
-    if param == 0:
-        f = open("./e.dat", "r")
+    global zeros_hash
+    if zero_index in zeros_hash:
+       return zeros_hash[zero_index]
     else:
-        f = open("./pi.dat", "r")
-    f.read(2)
-    position = 0
-    zero_index = 1
+       mp.prec = base
+       mp.dps = base
+       zero = str(zetazero(zero_index).imag)
+       idx = zero.index(".")
+       zero = zero[idx + 1:]
+       zeros_hash[zero_index] = zero
+       return zero
+
+def factorize(num, base):
+    l = len(num)
+    snippet1 = ""
+    snippet2 = ""
+    ctr1 = 0
+    ctr2 = 0
+    f1 = open("./e.dat", "r")
+    f1.read(2)
+    f2 = open("./pi.dat", "r")
+    f2.read(2)
+    position1 = 0
+    zero_index1 = 1
+    position2 = 0
+    zero_index2 = 1
+    c = ""
+    z = ""
     while True:
-        position, ctr, is_the_same = get_next_block(num, 1 - param, position, ctr)
-        if is_the_same == 1:
-            zero_index = zero_index + 1
-            factor = factor + snippet
+        position1, ctr1, is_the_same1 = get_next_block(num, 1, position1, ctr1)
+        position2, ctr2, is_the_same2 = get_next_block(num, 0, position2, ctr2)
+        print("")
+        print("")
+        if is_the_same1 == 1:
+            zero_index1 = zero_index1 + 1
+            zero = get_zero(zero_index1, base)
+            z = zero[ctr1]
+            input(["1",c, z])
             continue
         else:
-            snippet = ""
-            f.seek(position)
-            zero = get_zero(zero_index, base)
+            snippet1 = ""
+            f1.seek(position1)
+            zero = get_zero(zero_index1, base)
             cnt = 0
-            z = len(zero)
-            n87 = 0
-            while True:
-                c = str(f.read(1))
-                d = zero[cnt]
-                cnt = cnt + 1
-                if cnt >= z:
-                    print("Out of Precision Exception !!")
-                    sys.exit(2)
-                b87c = either_8_or_7(c)
-                b87d = either_8_or_7(d)
-                if  b87c == True and  b87d == True:
-                    if c == '8' and d == '7' and n87 > 0:
-                        snippet = str(bin(n87)[2:]) 
-                        if param == 1:
-                            factor = factor + snippet[::-1]
-                        else:
-                            factor = factor + snippet
-                        if param == 0:
-                            decimal = int(factor[::-1], 2)
-                            decision, possible = divides(num, decimal)
-                            if decision == True: 
-                                 factors[param] = decimal
-                                 f.close()
-                                 return
-                            elif possible == False:
-                                 factors[param] = 0
-                                 f.close()
-                                 return
-                        elif param == 1:
-                           decimal = int(factor, 2)
-                           if decimal == int(num):
-                               factors[param] = decimal
-                               f.close()
-                               return
-                           elif decimal > int(num):
-                               factors[param] = 0
-                               f.close()
-                               return
-                        input(["param", param, "factor", factor, "n87", n87, "ctr", ctr])
-                    break
-                elif b87c == True:
-                    n87 = n87 + 1 
-            zero_index = zero_index + 1
+            c = str(f1.read(l))[ctr1]
+            z = zero[ctr1]
+            input(["1",c, z])
+            zero_index1 = zero_index1 + 1
+        if is_the_same2 == 1:
+            zero_index2 = zero_index2 + 1
+            zero = get_zero(zero_index2, base)
+            z = zero[ctr1]
+            input(["2",c, z])
+            continue
+        else:
+            snippet2 = ""
+            f2.seek(position2)
+            zero = get_zero(zero_index2, base)
+            cnt = 0
+            c = str(f1.read(l))[ctr2]
+            z = zero[ctr2]
+            input(["2", c, z])
+            zero_index2 = zero_index2 + 1
--- intfact.py
+++ intfact.py
@@ -5,18 +5,9 @@ from factorize import factorize
 
 if __name__ == "__main__":
      num = str(sys.argv[1])
-     factors = dict([])
-     factors[0] = 0
-     factors[1] = 0
-     prec = 256
-     t1 = threading.Thread(target=factorize, args=(num, 0, factors, prec,))
-     t2 = threading.Thread(target=factorize, args=(num, 1, factors, prec,))
-     t1.start()
-     t2.start()
-     t1.join()
-     t2.join()
-     input(["factor0", factors[0], "factor1", factors[1]])
-     if factors[1] == int(num):
-         print(str(factors[0]) + " is a factor of " + str(num))  
+     prec = 512
+     factor=factorize(num, prec)
+     if factor > 0:
+         print(str(factor) + " is a factor of " + str(num))  
      else:
          print(num + " is a Prime Number.")
