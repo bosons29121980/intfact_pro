e532eb7b8a2029a6522a7fa4942240bb6ac334c1
Subhendra Basu <basu.subhendra@outlook.com>
2023-03-26 10:18:42 +0530

    code

--- get_next.py
+++ get_next.py
@@ -2,18 +2,18 @@
 import sys
 
 if __name__ == "__main__":
-    param = int(sys.argv[1])
-    f = ""
-    if param == 1:
-       f = open("./pi.dat","r")
-    else:
-       f = open("./e.dat","r") 
+    f = open("./pi.dat","r")
+    g = open("./e.dat","r")
     f.read(2)
+    g.read(2)
     while True:
-       k = int(input("get next?"))
-       if k == 1:
+       k = input("get next?")
+       if k=="" or int(k) == 1:
            c = f.read(8)
+           d = g.read(8)
            print(c)
+           print(d)
        else:
            break
     f.close()
+    g.close()
