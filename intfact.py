937e46aac94a47f0f3041f9c980c7809b6b7c30f
Subhendra Basu <basu.subhendra@outlook.com>
2023-03-27 20:54:49 +0530

    code

--- intfact.py
+++ intfact.py
@@ -15,12 +15,11 @@ def get_zero(x):
 
 def construct_next8(num, ctr):
     l = len(num)
-    old_ctr = (ctr + 1) % l
     n = ""
     for x in range(0, 8):
        n = n + num[ctr % l]
        ctr = (ctr + 1) % l
-    return old_ctr, ctr, n
+    return ctr, n
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
@@ -33,13 +32,13 @@ if __name__ == "__main__":
     while True:
            c = f.read(8)
            d = g.read(8)
-           old_ctr, ctr, n = construct_next8(num, ctr)
+           ctr, n = construct_next8(num, ctr)
            p = 0
            q = 0
            for x in num:
               p = p + c.count(x)
               q = q + d.count(x)
-           if (p + q) == 8 and p == 4:
+           if p == q and p == 4:
                zero, mantissa = get_zero(i)
                nr = 0
                dr = 0
@@ -50,8 +49,6 @@ if __name__ == "__main__":
                          dr = dr + 1
                if nr == 4 and nr == dr:
                    input([n, zero])
-               else:
-                   print(n, zero)
            i = i + 1
     f.close()
     g.close()
