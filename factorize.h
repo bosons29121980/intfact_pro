6cac5146ff6a4d9fb79e425c490ab202808fde0c
Subhendra Basu <basu.subhendra@outlook.com>
2023-03-31 12:23:08 +0530

    First draft of factorization code ready. En route to code freeze. Doomsday for RSA encryption systems is 1 step nearer. RIP Rivest Shamir Adelman Systems

--- factorize.h
+++ factorize.h
@@ -3,9 +3,14 @@
 #include "common.h"
 #include <string>
 using namespace std;
+const int MAGIC = 24;
+const int START = 2;
+const int PART = 3;
+
 void* factorize(void* arg) {
     struct func_arg* a = (struct func_arg*) arg;
     char* num = a->num;
+    unsigned long long int l = strlen(num);
     int param = a->param;
     FILE* fp = 0, *fe = 0;
     if (param) {
@@ -16,9 +21,36 @@ void* factorize(void* arg) {
        fe = fopen("./pi.dat", "r");
     }
     std::string factor = "";
+    unsigned long long int ctr = 0;
     while (1) {
+        char pp = 0, ee = 0;
+        fscanf(fp, "%c", &pp);
+        char n = num[ctr % l];
+        fscanf(fe, "%c", &ee);
+        unsigned int pos1 = ftell(fp);
+        unsigned int pos2 = ftell(fe);
+        int sum = (pp - '0') + (n - '0') + (ee - '0');
+        char pp2 = 0, ee2 = 0;
+        fscanf(fp, "%c", &pp2);
+        char n2 = num[(ctr + 1) % l];
+        fscanf(fe, "%c", &ee2);
+        sum += (pp2 - '0') + (n2 - '0') + (ee2 - '0');
+        if (sum == MAGIC) {
+           if (ctr % PART == 0) {
+                factor = factor + "1";
+                fseek(fp, pos1, SEEK_SET);
+                fseek(fe, START, SEEK_SET);
+           } else {
+                factor = factor + "0";
+                fseek(fp, pos1, SEEK_SET);
+                fseek(fe, pos2, SEEK_SET);
+           }
+        }
+        ctr++; 
     }
     char* ret = strdup((char*) factor.c_str());
+    fclose(fp);
+    fclose(fe);
     return ret;
 }
 #endif
